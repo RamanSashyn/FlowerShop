d8d22d16c5db304a9547aa9dfd0a6e5b9f2b93d0
VASILIYKAS <yoyoynv@gmail.com>
2025-03-26 14:10:48 +0500

    Заменил main_menu_kb на функцию get_start_keyboard

--- bot_data/keyboards.py
+++ bot_data/keyboards.py
@@ -1,10 +1,18 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-main_menu_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="💬 Консультация")],
-        [KeyboardButton(text="🌸 Заказать букет под желание")],
-        [KeyboardButton(text="📷 Посмотреть коллекцию")],
-    ],
-    resize_keyboard=True
-)
+
+def get_start_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(
+            text="💬 Консультация", 
+            callback_data="consultation"
+        )],
+        [InlineKeyboardButton(
+            text="📷 Посмотреть коллекцию",
+            callback_data="view_collection"
+        )],
+        [InlineKeyboardButton(
+            text="🌸 Заказать букет под желание",
+            callback_data="order_bouquet"
+        )],
+    ])
