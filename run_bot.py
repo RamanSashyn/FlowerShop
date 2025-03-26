93e47c253fb835082e5f7946d9659f1d9bbb39d6
VASILIYKAS <yoyoynv@gmail.com>
2025-03-26 12:56:30 +0500

    Токен бота убран в переменные окружения, добавлена кнопка меню

--- run_bot.py
+++ run_bot.py
@@ -1,20 +1,43 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import (
+    BotCommand,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 from bot_data.handlers import register_handlers
+from dotenv import load_dotenv
+from textwrap import dedent
+
+bot = Bot(token=os.environ['TG_BOT_TOKEN'])
+dispatcher = Dispatcher()
 
 
-BOT_TOKEN = "7946435543:AAEkCNi2cyx6bD7lcPFdZWHzj6wthYQarcM"
+async def set_menu_commands(bot: Bot):
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запустить бота")
+    ])
+
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+    load_dotenv()
 
-    register_handlers(dp)
+    if not bot.token:
+        print(dedent("""\
+            Ошибка: Не указан TG_BOT_TOKEN.
+            Убедитесь, что он задан в переменных окружения.
+        """))
+        return
+
+    await set_menu_commands(bot)
+
+    register_handlers(dispatcher)
 
     await bot.delete_webhook(drop_pending_updates=True)
     print("Бот запущен!")
-    await dp.start_polling(bot)
+    await dispatcher.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
